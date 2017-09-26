2adc71dca509ad46a8eaaad5d7fe57e2feea3dde
hliang12 <hao-lin.liang@dynatrace.com>
2017-09-26 10:40:57 +0100

    Add files via upload

--- Linux scripts/InstallJBoss7x.sh
+++ Linux scripts/InstallJBoss7x.sh
@@ -35,7 +35,7 @@ else
 	if [ "$2" = "32" ]; then 
 		echo $DATE_WITH_TIME "32 bit" | tee -a deploy.log
 	else 
-		echo $DATE_WITH_TIME "Inputted incorrect bitness argument" | te deploy.log
+		echo $DATE_WITH_TIME "Inputted incorrect bitness argument" | tee deploy.log
 		exit 1
 	fi
 fi
@@ -44,16 +44,20 @@ fi
 if [ -d "$1" ] && [ -d "$5" ]; then
 
 	if [ "$6" = "standalone" ]; then
-
+		echo $DATE_WITH_TIME "Inserting agent path for jboss standalone mode" | tee -a deploy.log 
 		echo  "JAVA_OPTS=\"\$JAVA_OPTS -agentpath:\""$1"/agent/lib"$BITNESS"/libdtagent.so\"=name="$4",server="$3"\"" >> "$5"/bin/standalone.conf
 
 	elif  [ "$6" = "domain" ]; then
-	
+		echo $DATE_WITH_TIME "Inserting agent path for jboss domain mode" | tee -a deploy.log 
+		echo $DATE_WITH_TIME "Checking if domain.xml cotainst <jvm-options>" | tee -a deploy.log 
 		if grep -q "<jvm-options>" ""$5"/domain/configuration/domain.xml"; then
 			#found add it into the jvm options part
+			
+			echo $DATE_WITH_TIME "Found <jvm-options> will add another <options> tag too it" | tee -a deploy.log 
 			sed -i "/<jvm-options>/a  <option value=\"-agentpath:\"$1/agent/lib/libdtagent.so\"=name=$4,server=$3\"/>"  $5/domain/configuration/domain.xml
 			
 			##check if sed worked 
+			echo $DATE_WITH_TIME "Check if added the options tag correctly" | tee -a deploy.log 
 			if grep -q "<option value=\"-agentpath:\"$1/agent/lib/libdtagent.so\"=name=$4,server=$3\"/>" "$5/domain/configuration/domain.xml" ; then
 				## added fine
 				echo $DATE_WITH_TIME "Added agent path options in fine"
@@ -65,18 +69,20 @@ if [ -d "$1" ] && [ -d "$5" ]; then
 			
 		else 
 			#not found
+			echo $DATE_WITH_TIME "No <jvm-options> tag, adding this in with <options> tag " | tee -a deploy.log 
 			 sed -i "/<heap .*>/a <jvm-options><option value=\"-agentpath:\"$1/agent/lib/libdtagent.so\"=name=$4,server=$3\"/> <jvm-options/>" $5/domain/configuration/domain.xml
 
 			 ##check if sed worked 
+			 echo $DATE_WITH_TIME "Checking if adding successfully" | tee -a deploy.log 
 			if grep -q "<jvm-options><option value=\"-agentpath:\"$1/agent/lib/libdtagent.so\"=name=$4,server=$3\"/> <jvm-options/>" "$5/domain/configuration/domain.xml"; then
 				## added fine
-				echo $DATE_WITH_TIME "Added agent path options in fine"
+				echo $DATE_WITH_TIME "Added agent path options in fine"| tee -a deploy.log 
 			else 
 				##failed 
-				echo $DATE_WITH_TIME "Failed to add the agent"
+				echo $DATE_WITH_TIME "Failed to add the agent" | tee -a deploy.log 
 				exit 1
 			fi
-		fi
+		fi 
 		
 		echo $DATE_WITH_TIME "Restarting jboss services" | tee -a deploy.log 
 		if [[ $6 == *".sh" ]]; then
